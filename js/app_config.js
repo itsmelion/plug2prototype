c96a2b5d0075d2c96838de2a0b17045a4be12b22
Christhopher Lion <christhopherleao@icloud.com>
2016-03-17 18:36:54 -0300

    Adjusted Plug Color Palette, and improvements on containers and alignment

--- js/app_config.js
+++ js/app_config.js
@@ -2,42 +2,57 @@ angular.module('PlugDawnbreak', ['ngMaterial'])
 var app = angular.module('PlugDawnbreak', ['ngMaterial']);
 app.config(function($mdThemingProvider) {
 
+    //create yr own palette
+    $mdThemingProvider.definePalette('plug', {
+        '50': 'E0F7FA',   // #E0F7FA
+        '100': 'B2EBF2',  // #B2EBF2
+        '200': '80DEEA',  // #80DEEA
+        '300': '81D4FA',  // #81D4FA
+        '400': '4FC3F7',  // #4FC3F7
+        '500': '29B6F6',  // #29B6F6
+        '600': '03A9F4',  // #03A9F4
+        '700': '039BE5',  // #039BE5
+        '800': '0288D1',  // #0288D1
+        '900': '0277BD',  // #0277BD
+        'A100': 'B2EBF2', // #B2EBF2
+        'A200': '80DEEA', // #80DEEA
+        'A400': '4DD0E1', // #4DD0E1
+        'A700': '00BCD4', // #00BCD4
+        'contrastDefaultColor': 'light',    // whether, by default, text(contrast) on this palette should be dark or light
+        'contrastDarkColors': ['50', '100','A100'], //hues which contrast should be 'dark' by default
+        'contrastLightColors': 'light'    // could also specify this if default was 'dark'
+    });
+
+    $mdThemingProvider.definePalette('Success', {
+        '50': 'E0F2F1',  // #E0F2F1
+        '100':'B2DFDB',  // #B2DFDB
+        '200':'80CBC4',  // #80CBC4
+        '300':'4DB6AC',  // #4DB6AC
+        '400':'26A69A',  // #26A69A
+        '500':'009688',  // #009688
+        '600':'00897B',  // #00897B
+        '700':'00796B',  // #00796B
+        '800':'00695C',  // #00695C
+        '900':'004D40',  // #004D40
+        'A100':'B2DFDB', // #B2DFDB
+        'A200':'80CBC4', // #80CBC4
+        'A400':'4DB6AC', // #4DB6AC
+        'A700':'009688', // #009688
+        'contrastDefaultColor': 'light',    // whether, by default, text(contrast) on this palette should be dark or light
+        'contrastDarkColors': ['50', '100' , 'A100' ], //hues which contrast should be 'dark' by default
+        'contrastLightColors': 'light'    // could also specify this if default was 'dark'
+    });
+
     $mdThemingProvider.theme('default')
-          .primaryPalette('blue')
-          .accentPalette('orange')
+          .primaryPalette('plug')
+          .accentPalette('Success')
           .warnPalette('red')
           .backgroundPalette('grey');
 
-    $mdThemingProvider.theme('custom')
-          .primaryPalette('grey')
-          .accentPalette('deep-purple')
-          .warnPalette('green')
-
-    //create yr own palette
-    $mdThemingProvider.definePalette('amazingPaletteName', {
-        '50': 'ffebee',
-        '100': 'ffcdd2',
-        '200': 'ef9a9a',
-        '300': 'e57373',
-        '400': 'ef5350',
-        '500': 'f44336',
-        '600': 'e53935',
-        '700': 'd32f2f',
-        '800': 'c62828',
-        '900': 'b71c1c',
-        'A100': 'ff8a80',
-        'A200': 'ff5252',
-        'A400': 'ff1744',
-        'A700': 'd50000',
-        'contrastDefaultColor': 'light',    // whether, by default, text         (contrast)
-                                    // on this palette should be dark or light
-        'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-         '200', '300', '400', 'A100'],
-        'contrastLightColors': undefined    // could also specify this if default was 'dark'
-    });
-
-   $mdThemingProvider.theme('custom2')
-        .primaryPalette('amazingPaletteName')
+    $mdThemingProvider.theme('plugPurple')
+          .primaryPalette('deep-purple')
+    $mdThemingProvider.theme('plugOrange')
+          .primaryPalette('deep-orange')
 
 });
 
